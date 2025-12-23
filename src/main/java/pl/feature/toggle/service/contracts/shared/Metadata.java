a445dbc92ccbe6f8f287194f958dda79fd712a26
SaQie <Kamcio99@interia.com>
2025-12-23 19:39:25 +0100

    added metadata to events

--- src/main/java/pl/feature/toggle/service/contracts/shared/Metadata.java
+++ src/main/java/pl/feature/toggle/service/contracts/shared/Metadata.java
@@ -1,6 +1,7 @@
 package pl.feature.toggle.service.contracts.shared;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public record Metadata(
         String actorId,
@@ -8,11 +9,13 @@ public record Metadata(
         LocalDateTime occurredAt
 ) {
 
+    private static final ChronoUnit PRECISION = ChronoUnit.SECONDS;
+
     public static Metadata create(String actorId, String username) {
-        return new Metadata(actorId, username, LocalDateTime.now());
+        return new Metadata(actorId, username, LocalDateTime.now().truncatedTo(PRECISION));
     }
 
     public static Metadata empty() {
-        return new Metadata(null, null, LocalDateTime.now());
+        return new Metadata(null, null, LocalDateTime.now().truncatedTo(PRECISION));
     }
 }
